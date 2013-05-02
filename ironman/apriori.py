4549c4535b454e6d0847f71465f1cdee16c94838
Kang HyoJun <admire9@gmail.com>
2013-05-02 18:47:55 +0900

    Implement ironman.apriori.get_frequency_for

--- ironman/apriori.py
+++ ironman/apriori.py
@@ -1,22 +1,46 @@
+import itertools
+
 import ironman.functions
 
-class FrequentItemMapReducer:
-    
-    def mapper(self, data):
-        for t_set in data:
-            for item in t_set:
-                yield (set(item).__str__(), 1)
+class MapReducer:
 
     def reducer(self, grouped_data):
         for k, v in grouped_data:
             yield (k, sum(v))
 
+    
+
+class FrequentItemMapReducer(MapReducer):
+    
+    def mapper(self, data):
+        for t_set in data:
+            for item in t_set:
+                yield (set(item).__str__(), 1)
+ 
+class FrequencyForItemMapReducer(MapReducer):
+    
+    def __init__(self, for_):
+        self.for_ = for_
+
+    def mapper(self, data):
+        for d in data:
+            for x in self.for_:
+                if x.issubset(d):
+                    yield (x.__str__(), 1)
+
 def get_frequency(transaction, minimum_support):
     r = ironman.functions.map_reduce(transaction, FrequentItemMapReducer())
     return dict(filter(lambda item: item[1] >= minimum_support, r))
 
-def get_frequency_for(for_, transaction, minimum_support):
-    r = ironman.functions.map_reduce(transaction, FrequentItemMapReducer())
+def get_frequency_for(for_, transaction, level, minimum_support):
+    subset = []
+    for x in itertools.combinations(for_, level):
+        subset.append(set(x))
+
+    r = ironman.functions.map_reduce(
+      transaction,
+      FrequencyForItemMapReducer(subset)
+    )
     return dict(filter(lambda item: item[1] >= minimum_support, r))
 
 def get_association_rules(data):
--- tests/test_functions.py
+++ tests/test_functions.py
@@ -17,4 +17,13 @@ class IronmanFunctionsTestCase(unittest.TestCase):
         
     def test_get_frequency(self):
         result = ironman.apriori.get_frequency(self.transaction, 2)
-        self.assertEqual(result, {"{'a'}": 3, "{'b'}": 6, "{'c'}": 4, "{'d'}": 5} , "Get frequency test")
+        self.assertEqual(result, {"{'a'}": 3, "{'b'}": 6, "{'c'}": 4, "{'d'}": 5}, "Get frequency test")
+
+    def test_get_frequency_for(self):
+        frequency = ironman.apriori.get_frequency(self.transaction, 2)
+        expected_set = {'a', 'b', 'c', 'd'}
+        result_set = ironman.functions.get_set_from_keys(frequency)
+        self.assertEqual(result_set, expected_set, "Get sets from dict keys")
+        r = ironman.apriori.get_frequency_for(expected_set, self.transaction, 2, 2)
+        for k, v in r.items():
+            self.assertTrue(v >= 2)
