796177d37025712b698d173f6f6799950f6e946b
Kang HyoJun <admire9@gmail.com>
2013-05-06 18:05:45 +0900

    Add setup.py to ditribute a module

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='ironman',
+    version='0.1',
+    description='Implementation of Apriori Algorithm in python3',
+    author='Kang Hyojun',
+    author_email='hyojun@admire.kr',
+    packages=['ironman'],
+    package_dir={'ironman': 'ironman'}
+)
