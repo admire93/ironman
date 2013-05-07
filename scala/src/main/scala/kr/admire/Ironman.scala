d2f4fa90374f0c6354a646abd1446c8559780227
Kang HyoJun <admire9@gmail.com>
2013-05-08 00:17:10 +0900

    Implement Ironman.getFrequency

--- scala/src/main/scala/kr/admire/Ironman.scala
+++ scala/src/main/scala/kr/admire/Ironman.scala
@@ -1,4 +1,34 @@
 package kr.admire
 
+object MapReduce {
+  def collect[T](mappedData: List[(Set[T], Long)]): Map[Set[T], List[Long]] = {
+    return mappedData.groupBy(_._1).mapValues(_.map(_._2))
+  }
+
+  def apply[T](data: List[Set[T]], mapReducer: MapReducer): Map[Set[T], Long] = {
+    return mapReducer.reducer(collect(mapReducer.mapper(data)))
+  }
+}
+
+abstract class MapReducer {
+  def mapper[T](data: List[Set[T]]): List[(Set[T], Long)]
+
+  def reducer[T](mappedData: Map[Set[T], List[Long]]): Map[Set[T], Long] = mappedData.mapValues(_.reduce(_ + _))
+}
+
+class FrequencyItemMapReducer extends MapReducer {
+  override def mapper[T](data: List[Set[T]]): List[(Set[T], Long)] = {
+    val res: Iterable[(Set[T], Long)] = for {
+      setData <- data
+      item <- setData
+    } yield (Set[T](item), 1: Long)
+
+    return res.toList
+  } 
+}
+
 object Ironman {
+  def getFrequency[T](data: List[Set[T]]): Map[Set[T], Long] = {
+    return MapReduce(data , new FrequencyItemMapReducer())
+  }
 }
--- scala/src/test/scala/kr/admire/IronmanSpec.scala
+++ scala/src/test/scala/kr/admire/IronmanSpec.scala
@@ -1,10 +1,27 @@
 import org.specs2.mutable._
+
 import kr.admire.Ironman
 
 class IronmanSpec extends Specification {
+  val transaction: List[Set[String]] = List[Set[String]](
+    Set("a", "b", "c", "d"),
+    Set("a", "b"),
+    Set("b", "c", "d"),
+    Set("b", "c"),
+    Set("a", "b", "d"),
+    Set("c", "d"),
+    Set("b", "d")
+  )
+
   "Ironman " should {
+    
     "get frequency table " in {
-        true === true
+      Ironman.getFrequency[String](transaction) === Map[Set[String], Long](
+                                                      Set("a") -> 3,
+                                                      Set("b") -> 6,
+                                                      Set("c") -> 4,
+                                                      Set("d") -> 5
+                                                    ) 
     }
   }
 }
